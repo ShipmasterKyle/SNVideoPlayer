023e71d26c5a639e81874bb912bc496799bd30f0
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2021-12-27 15:20:36 -0500

    Updated index.js to Scratch 3.0

--- index.js
+++ index.js
@@ -1,43 +1,84 @@
-(function(ext) {
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
- 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
-ext.playaudiofromyoutube = function(videoid,starttime,endtime){
-function createFrame(vid,start,end) {
-        var ifrm = document.createElement("iframe");
-ifrm.width = 0;
-ifrm.id = "youtube"
-        ifrm.height = 0;
-ifrm.src="https://www.youtube.com/embed/"+ vid +"?start="+start+"&end="+end+";autoplay=true"
-        ifrm.frameBorder="0"
-        ifrm.allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        document.body.appendChild(ifrm);
+class SNVideo {
+    getInfo(){
+        return {
+            "id": "SNVideo",
+            "name": "Video Player",
+            "blocks": [{
+                "opcode": "play", //This will be the ID code for the block
+                "blockType": "command",
+                "text": "Play video [id] with embed size [x] , [y]",
+                "arguments":{
+                    "id": {
+                        "type": "string",
+                        "defaultValue": "YouTube Video ID",
+                    },
+                    "x": {
+                        "type": "number",
+                        "defaultValue": "480"
+                    },
+                    "y": {
+                        "type": "number",
+                        "defaultValue": "360"
+                    }
+                },
+            ],
+            "menus": {
+            }
+        };
+      play({id,x,y}) {
+          var ifrm = document.createElement("iframe");
+          ifrm.width = x;
+          ifrm.height = y;
+          ifrm.id = "youtube"
+          ifrm.src="https://www.youtube.com/embed/"+ id
+          ifrm.frameBorder="0"
+          ifrm.allow="autoplay"
+          document.body.appendChild(ifrm);
+      };
     }
-        createFrame(videoid,starttime,endtime)
-};
-ext.stopyoutubeaudio = function(){
-function removeElement(elementId) {
-    var getelement = document.getElementById(elementId);
-    getelement.parentNode.removeChild(getelement);
 }
-removeElement("youtube")
-};
+Scratch.extensions.register(new SNVideo());
+
+// (function(ext) {
+//     // Cleanup function when the extension is unloaded
+//     ext._shutdown = function() {};
+ 
+//     // Status reporting code
+//     // Use this to report missing hardware, plugin or unsupported browser
+//     ext._getStatus = function() {
+//         return {status: 2, msg: 'Ready'};
+//     };
+// ext.playaudiofromyoutube = function(videoid,starttime,endtime){
+// function createFrame(vid,start,end) {
+        
+// ifrm.width = 0;
+// ifrm.id = "youtube"
+//         ifrm.height = 0;
+// ifrm.src="https://www.youtube.com/embed/"+ vid +"?start="+start+"&end="+end+";autoplay=true"
+//         ifrm.frameBorder="0"
+//         ifrm.allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+//         document.body.appendChild(ifrm);
+//     }
+//         createFrame(videoid,starttime,endtime)
+// };
+// ext.stopyoutubeaudio = function(){
+// function removeElement(elementId) {
+//     var getelement = document.getElementById(elementId);
+//     getelement.parentNode.removeChild(getelement);
+// }
+// removeElement("youtube")
+// };
  
  
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-[' ','play audio from youtube video ID: %s start time: %n end time: %n','playaudiofromyoutube'],
-[' ','stop youtube audio','stopyoutubeaudio'],
-        ],
-        url: 'https://boomerscratch.github.io/javascripts/youtubeaudioextensiondocumentation.mp4'
-    };
+//     // Block and block menu descriptions
+//     var descriptor = {
+//         blocks: [
+// [' ','play audio from youtube video ID: %s start time: %n end time: %n','playaudiofromyoutube'],
+// [' ','stop youtube audio','stopyoutubeaudio'],
+//         ],
+//         url: 'https://boomerscratch.github.io/javascripts/youtubeaudioextensiondocumentation.mp4'
+//     };
  
-    // Register the extension
-    ScratchExtensions.register('Youtube audio', descriptor, ext);
-})({});
+//     // Register the extension
+    
+// })({});
